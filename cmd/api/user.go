dca5645f915013f5462120c27513cecc258fc874
Pablo <Pablo.ArranzRopero@zooplus.com>
2019-02-12 16:59:09 +0100

    generating swagger documentation

--- cmd/api/user.go
+++ cmd/api/user.go
@@ -10,14 +10,11 @@ import (
 // @Description Creates an user
 // @Accept  json
 // @Produce  json
-// @Param user body string false "name search by q"
-// @Param password query string false "name search by q"
-// @Success 200 {array} model.Account
-// @Header 200 {string} Token "qwerty"
-// @Failure 400 {object} httputil.HTTPError
-// @Failure 404 {object} httputil.HTTPError
-// @Failure 500 {object} httputil.HTTPError
-// @Router /accounts [get]
+// @Param user body string true "username"
+// @Param password body string true "password"
+// @Success 200 {string} string "ok"
+// @Failure 400 {string} string "no ok"
+// @Router /user/new [get]
 func CreateUser(res http.ResponseWriter, req *http.Request) {
 	io.WriteString(res, "Create user")
 }
